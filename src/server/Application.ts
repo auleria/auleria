af7f2f3aa97bd20372cb5bcea8c4364116dc60e8
Rasmus Israelsson <israelssonrasmusraz@gmail.com>
2017-03-19 22:05:45 +0100

    Changed name from app.ts to Application.ts and installed peerjs

--- src/server/Application.ts
+++ src/server/Application.ts
@@ -1,4 +1,6 @@
 import * as express from "express";
+import * as http from "http";
+let ExpressPeerServer = require("peer").ExpressPeerServer;
 
 let title = "Hej";
 const app = express();
@@ -13,14 +15,33 @@ app.get("/", (req, res) => {
 	});
 });
 
+let server = http.createServer(app);
+
+let peerServer = ExpressPeerServer(server, {debug: true});
+app.use("/broker", peerServer);
+
+let peers = new Map<string, boolean>();
+
+peerServer.on("connection", (id : string) => {
+	peers.set(id, true);
+});
+
+peerServer.on("disconnect", (id : string) => {
+	peers.delete(id);
+});
+
+app.get("/clients", (req, res) => {
+	res.send(Array.from(peers.keys()));
+});
+
+server.listen(9080, () => {
+	console.log("Server is up and running on port", 9080);
+});
+
 app.use((req, res, next) => {
 	res.status(404).render("error", {
 		title: "Error: 404",
 		statusCode: 404,
 		message: "Not found."
 	});
-});
-
-app.listen(9080, null, () => {
-	console.log("Server is up and running, listening to port", 9080);
 });
