0ecfbb4393423d2787f5697598fd8780ef1b9e94
Rasmus Israelsson <israelssonrasmusraz@gmail.com>
2017-03-19 23:54:56 +0100

    adding Remote.once for properties we only want to send once. Adding IRemoteProperty

--- src/client/Remote.ts
+++ src/client/Remote.ts
@@ -3,7 +3,7 @@ enum DIRECTION {BI, SLAVE_TO_MASTER, MASTER_TO_SLAVE};
 export class Remote
 {
 	public static DIRECTION = DIRECTION;
-	private static list = new Map<string, Array<string>>();
+	private static list = new Map<string, Array<IRemoteProperty>>();
 
 	public static bi(targetClass : any, property : string)
 	{
@@ -20,6 +20,21 @@ export class Remote
 		Remote.set(targetClass, property, DIRECTION.MASTER_TO_SLAVE);
 	}
 
+	public static once(targetClass : any, property : string)
+	{
+		let properties = Remote.list.get(property);
+		if (properties)
+		{
+			properties.find((prop : IRemoteProperty) => {
+				if (prop.name === property)
+				{
+					prop.once = true;
+					return true;
+				}
+			});
+		}
+	}
+
 	public static getRemotes(target : any)
 	{
 		return Remote.list.get(target.constructor.name) || null;
@@ -28,8 +43,15 @@ export class Remote
 	private static set(targetClass : any, property : string, type : DIRECTION)
 	{
 		console.log("Keeping track of", property, "in all instances of type", targetClass.constructor.name);
-		let properties = Remote.list.get(targetClass.constructor.name) || new Array<string>();
-		properties.push(property);
+		let properties = Remote.list.get(targetClass.constructor.name) || new Array<IRemoteProperty>();
+		properties.push({name: property, once: false, hasOnceBeen: false});
 		Remote.list.set(targetClass.constructor.name, properties);
 	}
+}
+
+interface IRemoteProperty
+{
+	name : string;
+	once : boolean;
+	hasOnceBeen : boolean;
 }
