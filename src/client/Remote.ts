699fd37d2b2236fa847a4635aa5d912bb6d8e3b0
Rasmus Israelsson <israelssonrasmusraz@gmail.com>
2017-03-21 15:53:44 +0100

    revised and imporved Remote.
    - Remote.bi()
    - Remote.master()
    - Remote.slave()
    + Remote.monitor()
    + Remote.augmentClasses()
    + Remote.compileParents()
    + Remote.getProperties()
    + Remote.traverseProperty()
    + IPropertyPair()

--- src/client/Remote.ts
+++ src/client/Remote.ts
@@ -3,55 +3,108 @@ enum DIRECTION {BI, SLAVE_TO_MASTER, MASTER_TO_SLAVE};
 export class Remote
 {
 	public static DIRECTION = DIRECTION;
-	private static list = new Map<string, Array<IRemoteProperty>>();
+	private static list = new Map<any, IRemoteClass>();
 
-	public static bi(targetClass : any, property : string)
+	public static monitor(direction : DIRECTION, noConstructor = false)
 	{
-		Remote.set(targetClass, property, DIRECTION.BI);
+		return (targetClass : any, property : string) => {
+			Remote.set(targetClass, property, direction, noConstructor);
+		};
 	}
 
-	public static slave(targetClass : any, property : string)
+	private static set(targetClass : any, property : string, type : DIRECTION, noConstructor = false)
 	{
-		Remote.set(targetClass, property, DIRECTION.SLAVE_TO_MASTER);
+		let target = noConstructor ? targetClass : targetClass.constructor;
+		let remoteClass = Remote.list.get(target) || <IRemoteClass>{class: target, properties: new Array<IRemoteProperty>()};
+		let properties = remoteClass.properties;
+		properties.push({
+			name: property
+		});
+		Remote.list.set(target, remoteClass);
 	}
 
-	public static master(targetClass : any, property : string)
+	public static augmentClasses()
 	{
-		Remote.set(targetClass, property, DIRECTION.MASTER_TO_SLAVE);
+		Remote.monitor(DIRECTION.BI, true)(THREE.Vector3, "x");
+		Remote.monitor(DIRECTION.BI, true)(THREE.Vector3, "y");
+		Remote.monitor(DIRECTION.BI, true)(THREE.Vector3, "z");
+
+		Remote.monitor(DIRECTION.BI, true)(THREE.Quaternion, "x");
+		Remote.monitor(DIRECTION.BI, true)(THREE.Quaternion, "y");
+		Remote.monitor(DIRECTION.BI, true)(THREE.Quaternion, "z");
+		Remote.monitor(DIRECTION.BI, true)(THREE.Quaternion, "w");
 	}
 
-	public static once(targetClass : any, property : string)
+	public static compileParents()
 	{
-		let properties = Remote.list.get(property);
-		if (properties)
-		{
-			properties.find((prop : IRemoteProperty) => {
-				if (prop.name === property)
-				{
-					prop.once = true;
-					return true;
-				}
+		this.list.forEach((value, key) => {
+			let parents = Remote.getAllParents(value.class);
+			parents.forEach(parent => {
+				value.properties.push(...Remote.getRemotes(parent).properties);
 			});
+		});
+	}
+
+	public static getAllParents(target : any)
+	{
+		let current = target;
+		let allParents = new Array<Object>();
+		let prototype = current.prototype;
+		let parent = prototype.__proto__.constructor;
+		while (parent !== null && parent !== Function && parent !== Object)
+		{
+			allParents.push(parent);
+			current = parent;
+			prototype = current.prototype;
+			parent = prototype.__proto__.constructor;
 		}
+		return allParents;
 	}
 
 	public static getRemotes(target : any)
 	{
-		return Remote.list.get(target.constructor.name) || null;
+		return Remote.list.get(target) || null;
 	}
 
-	private static set(targetClass : any, property : string, type : DIRECTION)
+	public static getProperties(target : any) : IPropertyPair[]
 	{
-		console.log("Keeping track of", property, "in all instances of type", targetClass.constructor.name);
-		let properties = Remote.list.get(targetClass.constructor.name) || new Array<IRemoteProperty>();
-		properties.push({name: property, once: false, hasOnceBeen: false});
-		Remote.list.set(targetClass.constructor.name, properties);
+		let remotes = Remote.getRemotes(target.constructor);
+		if (remotes === null) { return null; }
+
+		let properties = new Array<IPropertyPair>(remotes.properties.length);
+		let i = 0;
+		remotes.properties.forEach(value => {
+			let property = Remote.traverseProperty(target[value.name]);
+			properties[i++] = {key: value.name, value: property};
+		});
+
+		return properties;
 	}
+
+	public static traverseProperty(property : any)
+	{
+		if (typeof property === "object")
+		{
+			let properties = Remote.getProperties(property);
+			return properties;
+		}
+		return property;
+	}
+}
+
+interface IRemoteClass
+{
+	class: string;
+	properties: Array<IRemoteProperty>;
 }
 
 interface IRemoteProperty
 {
 	name : string;
-	once : boolean;
-	hasOnceBeen : boolean;
+}
+
+export interface IPropertyPair
+{
+	key: string;
+	value: any;
 }
