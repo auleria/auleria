7c4d62b27bf762b5ba2ae2ee64ea56bf73b68e66
Rasmus Israelsson <israelssonrasmusraz@gmail.com>
2017-03-21 16:01:41 +0100

    compiling Remote properties and augmenting external classes. importing three.js in the webworker

--- src/client/Application.ts
+++ src/client/Application.ts
@@ -1,6 +1,8 @@
 import {GameWorker} from "./GameWorker";
 import {GameSlave} from "./GameSlave";
 import {GameMaster} from "./GameMaster";
+import {Remote} from "./Remote";
+
 
 let isWorker = () =>
 {
@@ -10,11 +12,18 @@ let isWorker = () =>
 if (isWorker())
 {
 	console.log("I'm a worker!");
+	console.log("Importing three.js...");
+	importScripts("/three/three.min.js");
+	console.log("Done!");
+	Remote.augmentClasses();
+	Remote.compileParents();
 	let worker = new GameWorker();
 }
 else
 {
 	console.log("I'm not a worker!");
+	Remote.augmentClasses();
+	Remote.compileParents();
 	let slave = new GameSlave();
 	let master = GameMaster.CreateFromWorker();
 
