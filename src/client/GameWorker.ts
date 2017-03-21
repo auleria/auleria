84c7ec7950082cdbdd8deaabcd713e5d4a304714
Rasmus Israelsson <israelssonrasmusraz@gmail.com>
2017-03-21 15:57:30 +0100

    implemented early GameWorker

--- src/client/GameWorker.ts
+++ src/client/GameWorker.ts
@@ -1,5 +1,28 @@
+import { GameWorld } from "./game/GameWorld";
+import { DebugWorld } from "./game/worlds/DebugWorld";
+import { Remote } from "./Remote";
 
 export class GameWorker
 {
+	private world : GameWorld;
 
+	constructor()
+	{
+		this.createWorld();
+	}
+
+	private sendMessage()
+	{
+
+	}
+
+	private createWorld()
+	{
+		this.world = new DebugWorld();
+		this.world.initialize();
+
+		let creationData = this.world.getCreationData();
+		console.log("World data:", creationData);
+		this.sendMessage();
+	}
 }
